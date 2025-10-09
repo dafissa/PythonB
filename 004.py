18bd1286e6ac7103ce2541f47fb21401436d24b0
Dafissa <rayssa.9258@colegioprogresso.g12.br>
2025-10-09 12:15:34 -0300

    Create 004.py

---
+++ 004.py
@@ -0,0 +1,19 @@
+print ("Qual seu gênero?")
+print ("Escolha e escreva um dos seguintes: ")
+print ("1 - Feminino")
+print ("2 - Masculino")
+print ("3 - Não binário")
+gender = input()
+if gender == ("Feminino"):
+    print ("OK!")
+    print ("Você selecionou feminino.")
+elif gender == ("Masculino"):
+    print ("Ok!")
+    print ("Você selecionou masculino.")
+elif gender == ("Não binário"):
+    print ("Ok!")
+    print ("Você selecionou não binário.")
+outro = int(input("Se nenhum dos acima, digite: "))
+    else: 
+        print ("Ok!")
+        print ("Você escolheu " + outro + ".")
