bd22baf3a2018ebec78b4f3ec1a8036e21cbbfaa
Dafissa <rayssa.9258@colegioprogresso.g12.br>
2025-10-29 08:38:19 -0300

    Implement basic calculator operations in 008.py

---
+++ 008.py
@@ -0,0 +1,27 @@
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))
+
+print("Operações: ")
+print("1 - adição")
+print("2 - subtração")
+print("3 - multiplicação")
+print("4 - divisão")
+
+operacao = input("Escolha uma operação: ")
+
+if (operacao == "1"):
+    resultado = num1 + num2
+    print(resultado)
+elif (operacao == "2"):
+    resultado = num1 - num2
+    print(resultado)
+elif (operacao == "3"):
+    resultado = num1 * num2
+    print(resultado)
+elif (operacao == "4"):
+    resultado = num1 / num2
+    print(resultado)
+    if (num2 != 0):
+        print(resultado)
+else:
+    print("Erro")
